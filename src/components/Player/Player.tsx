19b73854a8e67a8dc706cc8cb6edc64b0252a41d
ygorpinto <ygor.pinto@hotmail.com>
2021-06-10 19:14:53 -0300

    ok

--- src/components/Player/Player.tsx
+++ src/components/Player/Player.tsx
@@ -1,6 +1,13 @@
+import { useContext } from 'react';
+import { Context } from '../../utils/context';
 import PlayerStyles from './PlayerStyles'
 
 const Player = () => {
+
+    const {
+        state,
+        dispatch
+      } = useContext(Context);
 
     return (
         <PlayerStyles>
